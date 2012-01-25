7d2798204b2a781468e2ef1ede0f76c92393c375
Vlada Z <ng2m09@gmail.com>
2012-01-25 22:31:20 +0100

    initial state

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ovper::Application.config.secret_token = '2282ea3ca3939a223bdfe1cb1c2c71a395fd9749f7b59a271dc24b8dbf5c9400516afe648facfcaf30cbc23acf73a779b8a7f64573bcf23f04d2cab6449a1dab'
